ad4f826ec60094c3e0d263fb964134eab7f64816
stefan-berger_pace <stefan.berger@cariad.technology>
2025-12-16 07:39:16 +0100

    rename trip_duration_stats

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -83,7 +83,7 @@ def station_stats(df):
     print('-'*40)
 
 
-def trip_duration_stats(df):
+def print_trip_duration_stats(df):
     """Displays statistics on the total and average trip duration."""
 
     print('\nCalculating Trip Duration...\n')
