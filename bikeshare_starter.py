82ec886322bdcfe6f7f053a236b12b4018abd560
stefan-berger_pace <stefan.berger@cariad.technology>
2025-12-16 07:40:46 +0100

    BugFix: use newly renamed print_trip_duration_stats

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -125,7 +125,7 @@ def main():
 
         time_stats(df)
         station_stats(df)
-        trip_duration_stats(df)
+        print_trip_duration_stats(df)
         user_stats(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
